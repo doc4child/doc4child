eda92a5761b34ccddee79bed0acc33b28dda551c
Jaimin Patel <29556646+doc4child@users.noreply.github.com>
2024-05-14 15:18:13 -0500

    install packages.

--- Install packages necessory.R
+++
@@ -1,4 +0,0 @@
-install.packages("rmarkdown")
-install.packages("bookdown")
-install.packages("tinytex")
-tinytex::install_tinytex()
--- Install packages.R
+++ Install packages.R
@@ -8,6 +8,38 @@ p_load(rmarkdown, bookdown, servr)
 
 tinytex::install_tinytex()
 
+install.packages("rmarkdown")
+install.packages("bookdown")
+install.packages("tinytex")
+tinytex::install_tinytex()
+
+
+
+list.of.packages <- c(
+  "tidyverse",
+  "timetk",
+  "tibbletime",
+  "tidyquant",
+  "xts",
+  "lubridate",
+  "R6",
+  "here",
+  "rprojroot")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages, repos='https://cran.rstudio.com/ ', dependencies = TRUE)
+
+for (i.pack in 1:length(list.of.packages)){
+  suppressMessages(require(list.of.packages[i.pack], character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE))
+}
+
+update.packages(ask = FALSE)
+remotes::install_github("noamross/redoc")
+
+
+redoc::dedoc("R:/R/Bookdown/Manuscript.docx")
+
+
 
 # Debugging
 # If an error occurs when compiling a LaTeX to PDF, and the error message is not clear, please follow these steps one by one until the problem is gone.
